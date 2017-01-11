9db57f22a890e9289c80e2bdbdc1232f1edbd5c8
iamer <amer248@gmail.com>
2017-01-10 16:16:43 -0800

    initial commit of contoso.

---
+++ contoso/contoso/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace contoso.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hi from the Store Controller --> index page";
+        }
+        public string Browse()
+        {
+            return "Hi from the Store Controller --> browse page";
+        }
+        public string Details()
+        {
+            return "Hi from the Store Controller --> details page";
+        }
+    }
+}
